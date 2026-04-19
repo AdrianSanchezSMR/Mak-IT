1ab7599274ceee7d1e4c4c21116001951bdab3ef
Adrian Sanchez Rufian <148220867+AdrianSanchezSMR@users.noreply.github.com>
2026-04-19 15:49:38 +0200

    Prototipo Pruebas
    Spring Boot inicializado, conectado a MySQL y generando 2 tablas automáticamente

---
+++ app/src/main/java/com/makeit/app/HolaController.java
@@ -0,0 +1,13 @@
+package com.makeit.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+
+    @GetMapping("/hola")
+    public String hola() {
+        return "Servidor de Make It encendido y funcionando";
+    }
+}
